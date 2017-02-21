4b270d5a5d574a6d494a60f4811512eadb91c6cc
Denim Mazuki <denimmazuki@gmail.com>
2017-02-21 11:02:45 -0800

    Initial commit

---
+++ Maximizer/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  Maximizer
+//
+//  Created by Denim Mazuki on 2/10/17.
+//  Copyright © 2017 Denim Mazuki. All rights reserved.
+//
+
+import Foundation
---
+++ Maximizer/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Maximizer
+//
+//  Created by Denim Mazuki on 2/10/17.
+//  Copyright © 2017 Denim Mazuki. All rights reserved.
+//
+
+import Foundation
